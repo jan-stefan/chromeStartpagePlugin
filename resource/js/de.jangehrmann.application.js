d3b1e3d44f59a5f3e940df0a1d259ed27e3d7a2d
jgehrmann <mail@jangehrmann.de>
2017-05-10 23:31:06 +0200

    Initial commit.

---
+++ resource/js/de.jangehrmann.application.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var BrowserPreview = (function () {
+    function BrowserPreview() {
+    }
+    /**
+     * Draw this element to given id contained in html dom.
+     * @param Id
+     */
+    BrowserPreview.prototype.drawToId = function (Id) {
+    };
+    return BrowserPreview;
+}());
+exports.BrowserPreview = BrowserPreview;
---
+++ resource/js/de.jangehrmann.application.ts
@@ -0,0 +1,15 @@
+
+export class BrowserPreview{
+    private referenzId: number;
+    private template: string;
+
+
+
+    /**
+     * Draw this element to given id contained in html dom.
+     * @param Id
+     */
+    public drawToId(Id: string): void{
+
+    }
+}
